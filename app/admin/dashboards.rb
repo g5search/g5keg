096a08560a071d366b9b679ad0eec96148918301
Chris Kraybill <ckraybill@gmail.com>
2011-05-20 00:45:12 -0700

    Rotate a keg from the dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -31,5 +31,10 @@ ActiveAdmin::Dashboards.build do
   #   section "Recent User", :priority => 1
   #
   # Will render the "Recent Users" then the "Recent Posts" sections on the dashboard.
-
+  
+  section "Switch Keg" do
+    div do
+      render('/admin/dashboard/switch_keg', :model => 'dashboards')
+    end
+  end
 end
