907b44a5080490469dacff3176d434caf3bdb04c
Chris Kraybill <ckraybill@gmail.com>
2011-05-20 00:44:32 -0700

    Refactor kegs controller rotate

--- app/controllers/kegs_controller.rb
+++ app/controllers/kegs_controller.rb
@@ -1,6 +1,6 @@
 class KegsController < ApplicationController
   def rotate
-    new_keg = Keg.create(:beer_id => params[:beer_id])
+    new_keg = Keg.create(params[:keg])
     Keg.rotate(new_keg)
     flash[:notice] = 'Keg rotated'
     redirect_to :back
--- spec/controllers/kegs_controller_spec.rb
+++ spec/controllers/kegs_controller_spec.rb
@@ -5,22 +5,23 @@ describe KegsController do
     before do
       @request.env['HTTP_REFERER'] = '/admin'
       @old_keg = Factory.create(:keg)
+      @params = { :keg => { :beer_id => @old_keg.beer.id } }
     end
     it 'creates a new keg' do
-      expect {post :rotate, :beer_id => @old_keg.beer.id}.to change {Keg.count}.by(1)
+      expect {post :rotate, @params}.to change {Keg.count}.by(1)
     end
     it 'rotates the new keg in' do
       @a_new_keg = Factory.create(:keg)
       Keg.stub(:create).and_return(@a_new_keg)
-      post :rotate, :beer_id => @old_keg.beer.id
+      post :rotate, @params
       Keg.current.should == @a_new_keg
     end
     it 'flashes success' do
-      post :rotate, :beer_id => @old_keg.beer.id
+      post :rotate, @params
       flash[:notice].should_not be_empty
     end
     it 'redirects back' do
-      post :rotate, :beer_id => @old_keg.beer.id
+      post :rotate, @params
       response.should be_redirect
     end
   end
