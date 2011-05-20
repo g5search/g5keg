bbb25e8f2e06bc799d7de21f71df3e8801ec61f7
Chris Kraybill <ckraybill@gmail.com>
2011-05-19 21:04:57 -0700

    Delegate methods from keg to beer

--- app/models/keg.rb
+++ app/models/keg.rb
@@ -1,6 +1,8 @@
 class Keg < ActiveRecord::Base
   belongs_to :beer
   scope :on_tap, where('tapped is not null and kicked is null')
+  validates_presence_of :beer
+  delegate :name, :description, :ibus, :alcohol, :logo_path, :to => :beer
   def self.rotate(new_keg)
     if old_keg=self.on_tap.first
       old_keg.update_attribute(:kicked, Date.today)
--- spec/models/keg_spec.rb
+++ spec/models/keg_spec.rb
@@ -1,23 +1,43 @@
 require 'spec_helper'
 
 describe Keg do
+  before do
+    @a_brewery = Brewery.create(:name => 'Deschutes')
+    @a_beer = Beer.create(:name => 'Meh', 
+      :brewery => @a_brewery,
+      :description => 'Really hoppy stuff')
+  end
   it 'belongs to a beer' do
     subject.should respond_to(:beer)
   end
+  it 'is invalid without a beer' do
+    subject.errors_on(:beer).should_not be_empty
+  end
   it "has a base keg weight through it's beer's brewery"
+  describe 'delegates' do
+    it 'name, description, ibus to beer' do
+      a_keg = Keg.new(:beer => @a_beer)
+      [:name,:description,:ibus,:alcohol].each do |method|
+        a_keg.send(method).should == @a_beer.send(method)
+      end
+    end
+  end
   describe 'Keg#on_tap' do
     it 'is on tap if tapped and not kicked' do
-      a_tapped_keg = Keg.create(:tapped => Date.today)
+      a_tapped_keg = Keg.create(:tapped => Date.today,
+        :beer => @a_beer)
       Keg.on_tap.should include(a_tapped_keg)
     end
     it 'is not on tap if tapped and kicked' do
-      a_finished_keg = Keg.create(:tapped => Date.today, :kicked => Date.today+1)
+      a_finished_keg = Keg.create(:tapped => Date.today,
+        :kicked => Date.today+1,
+        :beer => @a_beer)
       Keg.on_tap.should_not include(a_finished_keg)
     end
   end
   describe '#rotate' do
     before do
-      @a_new_keg = Keg.create
+      @a_new_keg = Keg.create(:beer => @a_beer)
     end
     it 'taps the new kegs' do
       Keg.rotate(@a_new_keg)
@@ -28,12 +48,12 @@ describe Keg do
       @a_new_keg.reload.tapped.should == Date.today
     end
     it 'kicks the old keg' do
-      old_keg = Keg.create(:tapped => Date.today-1)
+      old_keg = Keg.create(:tapped => Date.today-1, :beer => @a_beer)
       Keg.rotate(@a_new_keg)
       old_keg.reload.should be_kicked
     end
     it 'populates the kicked date with todays date on the old keg' do
-      old_keg = Keg.create(:tapped => Date.today-1)
+      old_keg = Keg.create(:tapped => Date.today-1, :beer => @a_beer)
       Keg.rotate(@a_new_keg)
       old_keg.reload.kicked.should == Date.today
     end
