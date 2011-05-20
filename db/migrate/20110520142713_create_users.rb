1390ef45409658c4400b21f221cac9473c3388b2
Chris Kraybill <ckraybill@gmail.com>
2011-05-20 07:30:12 -0700

    Create a User class

---
+++ db/migrate/20110520142713_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :identity
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110520083228) do
+ActiveRecord::Schema.define(:version => 20110520142713) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -74,5 +74,14 @@ ActiveRecord::Schema.define(:version => 20110520083228) do
 
   add_index "kegs", ["beer_id"], :name => "index_kegs_on_beer_id"
   add_index "kegs", ["tapped", "kicked"], :name => "index_kegs_on_tapped_and_kicked"
+
+  create_table "users", :force => true do |t|
+    t.string   "identity"
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
