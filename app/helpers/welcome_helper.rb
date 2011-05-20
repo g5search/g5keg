0c49f3b8924008ec92dd324dc9d2092cbd379c23
Chris Kraybill <ckraybill@gmail.com>
2011-05-20 12:59:15 -0700

    Make updated show in Pacific time

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -12,6 +12,6 @@ module WelcomeHelper
   end
 
   def last_updated(keg)
-    keg.updated_at.strftime('%m/%d/%y at %l:%M:%S %p UTC') rescue 'N/A'
+    (keg.updated_at-8.hours).strftime('%m/%d/%y at %l:%M:%S %p') rescue 'N/A'
   end
 end
