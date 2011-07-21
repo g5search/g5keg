32c9b4183410b62e5cae293ad601f14ef672a439
Chris Kraybill <ckraybill@gmail.com>
2011-07-21 16:44:43 -0700

    Update bucket

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -9,7 +9,12 @@ class Beer < ActiveRecord::Base
     :styles => { :medium => "190x270!" }, 
     :default_url => "/images/beers/blank.png",
     :storage => :s3,
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml"
+    :bucket => 'g5kegapp',
+    :path => "beers/:id/:style/:basename.:extension",
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }
   
   def rating_for_user(user)
     if rating=rated_by?(user)
